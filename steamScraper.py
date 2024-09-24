6f8c3a3405c1c652551232252821904c0b15b2cb
PetjaPaivarinta <Petja.Paivarinta@proton.me>
2024-09-24 10:12:35 +0300

    made a scrollbar, also made it a tkinter window

--- steamScraper.py
+++ steamScraper.py
@@ -3,6 +3,44 @@ from bs4 import BeautifulSoup
 import json
 import pandas as pd
 import csv
+import tkinter as tk
+from tkinter import *
+
+passScreen = tk.Tk()
+passScreen.geometry("1200x800")
+passScreen.title("Games")
+
+frame = tk.Frame(passScreen)
+frame.pack(fill=tk.BOTH, expand=True)
+
+canvas = tk.Canvas(frame)
+canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+scrollbar = Scrollbar(frame, orient=tk.VERTICAL, command=canvas.yview)
+scrollbar.pack ( side = tk.RIGHT, fill=tk.Y )
+
+canvas.configure(yscrollcommand=scrollbar.set)
+canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox('all')))
+
+content_frame = tk.Frame(canvas)
+canvas.create_window((0,0), window=content_frame, anchor='nw')
+
+col_names = ("Title", "Original Price", "Discounted Price", "Release Date", "Discount Percentage")
+
+for i, col_name in enumerate(col_names, start=1):
+    tk.Label(content_frame, text=col_name).grid(row=3, column=i, padx=40)
+
+with open("games_data.csv", "r", newline="") as passfile:
+    reader = csv.reader(passfile)
+    data = list(reader)
+
+entrieslist = []
+for i, row in enumerate(data, start=4):
+    for col in range(5):
+        tk.Label(content_frame, text=row[col]).grid(row=i, column=col+1)
+
+
+passScreen.mainloop()
 
 url = 'https://store.steampowered.com/search/results/?query&start=0&count=100&dynamic_data=&sort_by=_ASC&snr=1_7_7_7000_7&filter=topsellers&specials=1&infinite=1'
 
@@ -37,8 +75,6 @@ def parse(data):
 
         with open('games_data.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            # Write the header row
-            writer.writerow(['Title', 'Original Price', 'Discounted Price', 'Release Date', 'Discount Amount'])
             # Write all the game data rows
             writer.writerows(game_data)
 
